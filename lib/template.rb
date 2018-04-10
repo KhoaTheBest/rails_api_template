ab2d3cdfcf64c1e5a1452725e9c98e41294072fb
Khoa Le <khoalt25791@gmail.com>
2018-04-10 23:50:21 +0700

    Update template.rb

--- lib/template.rb
+++ lib/template.rb
@@ -1,6 +1,5 @@
 if apply_capistrano?
   empty_directory_with_keep_file 'lib/capistrano/tasks'
-  copy_file 'lib/capistrano/mb/templates/crontab.erb'
   copy_file 'lib/capistrano/mb/templates/maintenance.erb.html'
 end
 copy_file 'lib/tasks/coverage.rake'
